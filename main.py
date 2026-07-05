373edf42ad2a9180562a6eb29dc76934a7104874
Щукин Егор <egorshukin1235@gmail.com>
2026-07-05 18:30:49 +0700

    Improve variant generation system

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, InputMediaPhoto
 from PIL import Image, ImageDraw, ImageFont
@@ -12,16 +14,7 @@ with open('token.txt', 'r') as f:
 
 bot=telebot.TeleBot(token)
 
-correct_answers = ["26", "xzy", "801", "14", "19", "17", "10", "270", "3094", "64", "94", "28", "192", "3", "23", "120", "180 190360573", "1911 178", "43", "2324324445", "25", "17", "13", "1004", "101075762 101417282 101588258 101645282", "568 50", "471228 49113954961677"]
-
-task_markup = InlineKeyboardMarkup()
-buttons = []
-buttons.append(InlineKeyboardButton(text="Предыдущее", callback_data="prev_task"))
-buttons.append(InlineKeyboardButton(text="Следующее", callback_data="next_task"))
-task_markup.row(*buttons)
-task_markup.add(InlineKeyboardButton(text="К списку заданий", callback_data="btn_start_exam"))
-task_markup.add(InlineKeyboardButton(text="Очистить мой выбор", callback_data="clear_answer"))
-task_markup.add(InlineKeyboardButton(text="Завершить экзамен", callback_data="finish_exam_warning_message"))
+#correct_answers = ["26", "xzy", "801", "14", "19", "17", "10", "270", "3094", "64", "94", "28", "192", "3", "23", "120", "180 190360573", "1911 178", "43", "2324324445", "25", "17", "13", "1004", "101075762 101417282 101588258 101645282", "568 50", "471228 49113954961677"]
 
 user_states = {}
 
@@ -34,6 +27,31 @@ conn = psycopg2.connect(
 )
 
 cursor = conn.cursor()
+
+#Создать markup с кнопками для навигации по заданиям
+def create_task_markup(task_number):
+    if task_number == 1:
+        prev_task_number = NUMBER_OF_TASKS
+    else:
+        prev_task_number = task_number - 1
+
+    if task_number == NUMBER_OF_TASKS:
+        next_task_number = 1
+    else:
+        next_task_number = task_number + 1
+
+    prev_task_callback = "task_" + str(prev_task_number)
+    next_task_callback = "task_" + str(next_task_number)
+    task_markup = InlineKeyboardMarkup()
+    buttons = []
+    buttons.append(InlineKeyboardButton(text="Предыдущее", callback_data=prev_task_callback))
+    buttons.append(InlineKeyboardButton(text="Следующее", callback_data=next_task_callback))
+    task_markup.row(*buttons)
+    task_markup.add(InlineKeyboardButton(text="К списку заданий", callback_data="btn_start_exam"))
+    task_markup.add(InlineKeyboardButton(text="Очистить мой выбор", callback_data="clear_answer"))
+    task_markup.add(InlineKeyboardButton(text="Завершить экзамен", callback_data="finish_exam_warning_message"))
+
+    return task_markup
 
 @bot.message_handler(commands=['start'])
 def send_main_menu_options(message):
@@ -59,6 +77,19 @@ def send_main_menu_options(message):
     for i in range(NUMBER_OF_TASKS):
         user_answers.append(None)
 
+    #Создание варианта из случайных заданий
+    task_variants = []
+    for i in range(NUMBER_OF_TASKS):
+        task_variants.append(random.randint(1, 3))
+
+    #Получение правильных ответов на вариант
+    correct_answers = []
+    for i in range(NUMBER_OF_TASKS):
+        path_to_answer = "tasks/" + str(i+1) + "/" + str(task_variants[i]) + "/answer.txt"
+        with open(path_to_answer, "r", encoding="utf-8") as f:
+            answer = f.read()
+            correct_answers.append(answer)
+
     user_states[user_id] = {
         "stage": "Menu",
         "current_task_number" : None,
@@ -67,7 +98,9 @@ def send_main_menu_options(message):
         "task_image_message_id": None,
         "task_answer_message_id": None,
         "task_answer_message_text": None,
-        "document_message_ids": []
+        "document_message_ids": [],
+        "task_variants": task_variants,
+        "correct_answers": correct_answers
     }
 
     markup = InlineKeyboardMarkup()
@@ -104,15 +137,25 @@ def send_task(call):
     task_number = int(call.data.split("_")[1])
     user_states[user_id]["current_task_number"] = task_number
     user_states[user_id]["stage"] = "task"
-    path_to_task_files = "tasks/" + str(task_number)
-    path_to_task_image = path_to_task_files + "/task" + str(task_number) + "_1.png"
-    with open(path_to_task_image, "rb") as photo:
-        image_message = bot.send_photo(user_id, photo, caption="Выполните задание и отправьте ответ на него")
-        user_states[user_id]["task_image_message_id"] = image_message.id
+
+    task_variant = user_states[user_id]["task_variants"][task_number - 1]
+    path_to_task_files = "tasks/" + str(task_number) + "/" + str(task_variant)
+    path_to_task_image = path_to_task_files + "/image.png"
+    path_to_task_text = path_to_task_files + "/task.txt"
+
+    with open(path_to_task_text, "r", encoding="utf-8") as file:
+        text = file.read()
+        text_message = bot.send_message(user_id, text=text)
+        user_states[user_id]["task_image_message_id"] = text_message.id
+
+    if os.path.isfile(path_to_task_image):
+        with open(path_to_task_image, "rb") as photo:
+            image_message = bot.send_photo(user_id, photo)
+            user_states[user_id]["task_image_message_id"] = image_message.id
 
     task_files = os.listdir(path_to_task_files)
     for filename in task_files:
-        if not filename.endswith(".png"):
+        if not ((filename == "image.png") or (filename == "task.txt") or (filename == "answer.txt")):
             path_to_file = path_to_task_files + "/" + filename
             with open(path_to_file, "rb") as file:
                 message = bot.send_document(user_id, file)
@@ -124,6 +167,7 @@ def send_task(call):
     else:
         message = "Ваш ответ: " + user_answer
 
+    task_markup = create_task_markup(task_number)
     user_states[user_id]["task_answer_message_text"] = message
     answer_message = bot.send_message(chat_id=user_id, reply_markup=task_markup, text=message)
     user_states[user_id]["task_answer_message_id"] = answer_message.id
@@ -134,6 +178,8 @@ def handle_task_input(message):
     user_id = message.from_user.id
     task_number = user_states.get(user_id).get("current_task_number")
     text = message.text
+
+    task_markup = create_task_markup(task_number)
 
     bot.edit_message_text(
         chat_id=user_id,
@@ -151,6 +197,7 @@ def clear_task_answer(call):
     user_id = call.from_user.id
     task_number = user_states[user_id]["current_task_number"]
 
+    task_markup = create_task_markup(task_number)
     bot.edit_message_text(
         chat_id=user_id,
         message_id=user_states[user_id]["task_answer_message_id"],
@@ -160,104 +207,6 @@ def clear_task_answer(call):
 
     user_states[user_id]["task_answer_message_text"] = "Вы не давали ответ на это задание"
     user_states[user_id]["user_answers"][task_number - 1] = None
-
-@bot.callback_query_handler(func=lambda call: call.data == "prev_task")
-def show_prev_task(call):
-    user_id = call.from_user.id
-    bot.answer_callback_query(call.id)
-
-    for message_id in user_states[user_id]["document_message_ids"]:
-        bot.delete_message(user_id, message_id)
-    user_states[user_id]["document_message_ids"] = []
-
-    task_number = int(user_states.get(user_id).get("current_task_number"))
-    if task_number == 1:
-        task_number = NUMBER_OF_TASKS
-    else:
-        task_number -= 1
-
-    path_to_new_task_image = "tasks/" + str(task_number) + "/task" + str(task_number) + "_1.png"
-    with open(path_to_new_task_image, "rb") as photo:
-        bot.edit_message_media(
-            media=InputMediaPhoto(photo),
-            chat_id=user_id,
-            message_id=user_states[user_id]["task_image_message_id"]
-        )
-
-    path_to_task_files = "tasks/" + str(task_number)
-    task_files = os.listdir(path_to_task_files)
-    for filename in task_files:
-        if not filename.endswith(".png"):
-            path_to_file = path_to_task_files + "/" + filename
-            with open(path_to_file, "rb") as file:
-                message = bot.send_document(user_id, file)
-                user_states[user_id]["document_message_ids"].append(message.id)
-
-    user_answer = user_states[user_id]["user_answers"][task_number - 1]
-    if user_answer is None:
-        message = "Вы не давали ответ на это задание"
-    else:
-        message = "Ваш ответ: " + user_answer
-
-    if user_states[user_id]["task_answer_message_text"] != message:
-        bot.edit_message_text(
-            chat_id=user_id,
-            message_id=user_states[user_id]["task_answer_message_id"],
-            reply_markup=task_markup,
-            text=message
-        )
-
-    user_states[user_id]["task_answer_message_text"] = message
-    user_states[user_id]["current_task_number"] = task_number
-
-@bot.callback_query_handler(func=lambda call: call.data == "next_task")
-def show_next_task(call):
-    user_id = call.from_user.id
-    bot.answer_callback_query(call.id)
-
-    for message_id in user_states[user_id]["document_message_ids"]:
-        bot.delete_message(user_id, message_id)
-    user_states[user_id]["document_message_ids"] = []
-
-    task_number = int(user_states.get(user_id).get("current_task_number"))
-    if task_number == NUMBER_OF_TASKS:
-        task_number = 1
-    else:
-        task_number += 1
-
-    path_to_new_task_image = "tasks/" + str(task_number) + "/task" + str(task_number) + "_1.png"
-    with open(path_to_new_task_image, "rb") as photo:
-        bot.edit_message_media(
-            media=InputMediaPhoto(photo),
-            chat_id=user_id,
-            message_id=user_states[user_id]["task_image_message_id"]
-        )
-
-    path_to_task_files = "tasks/" + str(task_number)
-    task_files = os.listdir(path_to_task_files)
-    for filename in task_files:
-        if not filename.endswith(".png"):
-            path_to_file = path_to_task_files + "/" + filename
-            with open(path_to_file, "rb") as file:
-                message = bot.send_document(user_id, file)
-                user_states[user_id]["document_message_ids"].append(message.id)
-
-    user_answer = user_states[user_id]["user_answers"][task_number - 1]
-    if user_answer is None:
-        message = "Вы не давали ответ на это задание"
-    else:
-        message = "Ваш ответ: " + user_answer
-
-    if user_states[user_id]["task_answer_message_text"] != message:
-        bot.edit_message_text(
-            chat_id=user_id,
-            message_id=user_states[user_id]["task_answer_message_id"],
-            reply_markup=task_markup,
-            text=message
-        )
-
-    user_states[user_id]["task_answer_message_text"] = message
-    user_states[user_id]["current_task_number"] = task_number
 
 @bot.callback_query_handler(func=lambda call: call.data ==  "finish_exam_warning_message")
 def send_finish_exam_warning_message(call):
@@ -280,7 +229,7 @@ def send_exam_results(call):
     user_answers = user_states.get(user_id).get("user_answers")
     primary_score = 0
     for task in range(1, NUMBER_OF_TASKS + 1):
-        if user_answers[task - 1] == correct_answers[task - 1]:
+        if user_answers[task - 1] == user_states[user_id]["correct_answers"][task - 1]:
             if (task >= 1) and (task <= 25):
                 primary_score += 1
             elif (task >= 26) and (task <= 27):
@@ -294,7 +243,7 @@ def send_exam_results(call):
     uncompleted_tasks_count = 0
 
     for task in range(1, NUMBER_OF_TASKS + 1):
-        if user_answers[task - 1] == correct_answers[task - 1]:
+        if user_answers[task - 1] == user_states[user_id]["correct_answers"][task - 1]:
             correct_tasks_count += 1
         elif user_answers[task - 1] is None:
             uncompleted_tasks_count += 1
@@ -323,7 +272,7 @@ def send_exam_results(call):
     #Заполнение таблицы ответами, покрас ячеек
     img = Image.open("results_empty.png")
     draw = ImageDraw.Draw(img)
-    font_text = ImageFont.truetype("arial.ttf", 14)
+    font_text = ImageFont.truetype("C:\\Windows\\Fonts\\arial.ttf", size=14)
     border_color = "#2D3436"
     correct_color = "#D4EDDA"
     wrong_color = "#F8D7DA"
@@ -335,7 +284,7 @@ def send_exam_results(call):
         y2 = 45 * (number + 2)
 
         user_answer = user_answers[number]
-        correct_answer = correct_answers[number]
+        correct_answer = user_states[user_id]["correct_answers"][number]
 
         if user_answer == correct_answer:
             color = correct_color
